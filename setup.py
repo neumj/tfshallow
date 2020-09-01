2a4570c1e39f17c905948d7e3a48120361584610
mjn <mjn@tripoli.local>
2020-09-01 13:12:45 -0700

    scaffold

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+reqs = [
+    "h5py",
+    "jupyterlab",
+    "matplotlib",
+    "numpy",
+    "pandas",
+    "pillow",
+    "scipy",
+    "scikit-learn",
+    "yaml"
+]
+
+conda_reqs = [
+    "h5py",
+    "jupyterlab",
+    "matplotlib",
+    "numpy",
+    "pandas",
+    "pillow",
+    "scipy",
+    "scikit-learn",
+    "yaml"
+]
+
+test_pkgs = []
+
+setup(
+    name="tfshallow",
+    python_requires='>3.4',
+    description="Package for tensor flow neural network experimentation",
+    url="https://github.com/neumj/neural-network",
+    install_requires=reqs,
+    conda_install_requires=conda_reqs,
+    test_requires=test_pkgs,
+    packages=find_packages(),
+    include_package_data=True
+)
